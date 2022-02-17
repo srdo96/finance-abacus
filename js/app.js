757f76d0815116579d1000fcee0692cc99cc1bf0
Sakib Rahman <seu.sakib@gmail.com>
2022-02-17 11:25:05 +0600

    add function for save button

--- js/app.js
+++ js/app.js
@@ -1,12 +1,32 @@
 function calculate() {
   //   debugger;
-  const foodInput = document.getElementById("food-input").value;
-  const rentInput = document.getElementById("rent-input").value;
-  const clothesInput = document.getElementById("clothes-input").value;
   const totalExpensesDisplay = document.getElementById("total-expenses");
-
-  const totalExpenses =
-    parseInt(foodInput) + parseInt(rentInput) + parseInt(clothesInput);
-  //   console.log(totalExpenses);
+  const balanceDisplay = document.getElementById("balance");
+  const income = getInputValue("income");
+  const rent = getInputValue("rent");
+  const food = getInputValue("food");
+  const clothes = getInputValue("clothes");
+  const totalExpenses = food + rent + clothes;
   totalExpensesDisplay.innerText = totalExpenses;
+  const balance = income - totalExpenses;
+  balanceDisplay.innerText = balance;
+}
+
+function getInputValue(name) {
+  const value = document.getElementById(name + "-input").value;
+  return parseInt(value);
+}
+
+function saveCalculation() {
+  const percentage = getInputValue("percentage");
+  const income = getInputValue("income");
+  const saveAmount = income * (percentage / 100);
+  const savingDisplay = document.getElementById("saving-display");
+  savingDisplay.innerText = saveAmount;
+
+  const balanceDisplay = document.getElementById("balance");
+  const balance = parseInt(balanceDisplay.innerText);
+  const remainingAmount = balance - saveAmount;
+  const remainingAmountDisplay = document.getElementById("remaining-amount");
+  remainingAmountDisplay.innerText = remainingAmount;
 }
